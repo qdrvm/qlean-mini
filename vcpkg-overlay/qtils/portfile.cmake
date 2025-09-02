7107ea1bcdbd23f60b1eec978b9720c6a6eb30c8
Dmitriy Khaustov aka xDimon <khaustov.dm@gmail.com>
2025-09-02 10:49:02 +0300

    patch qtils
    Signed-off-by: Dmitriy Khaustov aka xDimon <khaustov.dm@gmail.com>

--- vcpkg-overlay/qtils/portfile.cmake
+++ vcpkg-overlay/qtils/portfile.cmake
@@ -4,6 +4,8 @@ vcpkg_from_github(
   REPO qdrvm/qtils
   REF refs/tags/v0.1.3
   SHA512 09e759f82ce273b602ec851ed7b5bb5fe1e0471a35a9874d04190946a38b8cf5dcea0923af91db798c53c01970c7bfe3452fb956efd66e067ecbec3e0c99fb02
+  PATCHES
+    vcpkg.patch
 )
 vcpkg_cmake_configure(SOURCE_PATH "${SOURCE_PATH}")
 vcpkg_cmake_install()
