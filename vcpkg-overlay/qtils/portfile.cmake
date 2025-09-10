cf049f33efb21ab2cb1b7e3867d5fb14db06897d
turuslan <turuslan.devbox@gmail.com>
2025-09-10 17:47:39 +0500

    qtils tag
    Signed-off-by: turuslan <turuslan.devbox@gmail.com>

--- vcpkg-overlay/qtils/portfile.cmake
+++ vcpkg-overlay/qtils/portfile.cmake
@@ -2,8 +2,8 @@ vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
 vcpkg_from_github(
   OUT_SOURCE_PATH SOURCE_PATH
   REPO qdrvm/qtils
-  REF 4eb3f8024817d66932cec0c52e74e127c137a78a
-  SHA512 c02b90803a1cbf09dcb0e4707c84b3afdc83449d12ad1771e2918a3cdb40b8d01bda4f93fcb50491e35593fd060ec53c8a4b0b425dbb3df936a32312e5b99859
+  REF refs/tags/v0.1.4
+  SHA512 124f3711eb64df3a2e207bff8bf953ccc2dfa838f21da72a1cc77c8aec95def350e70607adf9d8e7123e56d5bffcf830052f607dfa12badc7efe463bd0be747c
 )
 vcpkg_cmake_configure(SOURCE_PATH "${SOURCE_PATH}")
 vcpkg_cmake_install()
