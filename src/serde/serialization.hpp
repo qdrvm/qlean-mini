dfcd9a6e305ebc7e3b4645b2d4c33bcff84b9332
turuslan <turuslan.devbox@gmail.com>
2025-09-22 14:52:15 +0500

    catch
    Signed-off-by: turuslan <turuslan.devbox@gmail.com>

--- src/serde/serialization.hpp
+++ src/serde/serialization.hpp
@@ -10,9 +10,21 @@
 
 #include <qtils/byte_vec.hpp>
 #include <qtils/bytes.hpp>
+#include <qtils/enum_error_code.hpp>
 #include <sszpp/ssz++.hpp>
 
 namespace lean {
+  enum class SszError {
+    DecodeError,
+  };
+  Q_ENUM_ERROR_CODE(SszError) {
+    using E = decltype(e);
+    switch (e) {
+      case E::DecodeError:
+        return "ssz decode error";
+    }
+    abort();
+  }
 
   // template <typename T>
   //  qtils::ByteVec encode(const T &v) {
@@ -52,8 +64,10 @@ namespace lean {
     try {
       return ssz::deserialize<T>(
           reinterpret_cast<std::span<std::byte> &>(data));
-    } catch (...) {
-      return std::make_error_code(std::errc::invalid_argument);
+    } catch (const std::out_of_range &) {
+      return outcome::failure(SszError::DecodeError);
+    } catch (const std::invalid_argument &) {
+      return outcome::failure(SszError::DecodeError);
     }
   }
 
@@ -66,23 +80,23 @@ namespace lean {
   }
 
   template <size_t N>
-  std::array<uint8_t, N>& as_u8(std::array<std::byte, N>& v) {
-    return reinterpret_cast<std::array<uint8_t, N>&>(v);
+  std::array<uint8_t, N> &as_u8(std::array<std::byte, N> &v) {
+    return reinterpret_cast<std::array<uint8_t, N> &>(v);
   }
 
   template <size_t N>
-  std::array<std::byte, N>& as_byte(std::array<uint8_t, N>& v) {
-    return reinterpret_cast<std::array<std::byte, N>&>(v);
+  std::array<std::byte, N> &as_byte(std::array<uint8_t, N> &v) {
+    return reinterpret_cast<std::array<std::byte, N> &>(v);
   }
 
   template <size_t N>
-  std::array<uint8_t, N> as_u8(std::array<std::byte, N>&& v) {
-    return reinterpret_cast<std::array<uint8_t, N>&>(v);
+  std::array<uint8_t, N> as_u8(std::array<std::byte, N> &&v) {
+    return reinterpret_cast<std::array<uint8_t, N> &>(v);
   }
 
   template <size_t N>
-  std::array<std::byte, N> as_byte(std::array<uint8_t, N>&& v) {
-    return reinterpret_cast<std::array<std::byte, N>&>(v);
+  std::array<std::byte, N> as_byte(std::array<uint8_t, N> &&v) {
+    return reinterpret_cast<std::array<std::byte, N> &>(v);
   }
 
 
