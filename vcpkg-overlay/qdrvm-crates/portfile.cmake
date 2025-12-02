1cc4b06827778f880c2690c569481a532f165cea
kamilsa <kamilsa16@gmail.com>
2025-12-02 15:53:35 +0500

    Update qdrvm-crates to v1.0.6

--- vcpkg-overlay/qdrvm-crates/portfile.cmake
+++ vcpkg-overlay/qdrvm-crates/portfile.cmake
@@ -2,8 +2,8 @@ vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
 vcpkg_from_github(
   OUT_SOURCE_PATH SOURCE_PATH
   REPO qdrvm/qdrvm-crates
-  REF 83a22b5489d90cf24b276d29970dd425b9d5606e
-  SHA512 3a4194c9eade43c489b19cd6eda96e3d70fc0cb1e324ec32d2252675839a3c286d0a8f7694bb6782cb10c86951f8ccdaf3c075544cca3f9f3dcc484199d7826c
+  REF refs/tags/v1.0.6
+  SHA512 bede49cb4d42ada9daefee57b8b2ba7726231fcfbdfff28ed8c0f91fdb97cb56bfdc4baef611943454424668bdc9bc2fd9e284426934e19bb822452c10cf7be9
 )
 vcpkg_cmake_configure(SOURCE_PATH "${SOURCE_PATH}" OPTIONS
   "-DQDRVM_BIND_CRATES=c_hash_sig"
