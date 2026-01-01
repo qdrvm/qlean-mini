b4e2f51eb4547ca16de809355a5889cb1d5fe9c6
turuslan <turuslan.devbox@gmail.com>
2026-01-01 16:18:56 +0500

    debug fmt
    Signed-off-by: turuslan <turuslan.devbox@gmail.com>

--- src/utils/debug.hpp
+++ src/utils/debug.hpp
@@ -18,19 +18,44 @@ namespace lean {
 }  // namespace lean
 
 template <>
+struct fmt::formatter<lean::Debug<lean::AttestationData>> {
+  constexpr auto parse(const auto &ctx) {
+    return ctx.end();
+  }
+  auto format(const lean::Debug<lean::AttestationData> &v,
+              fmt::format_context &ctx) const {
+    return fmt::format_to(ctx.out(),
+                          "[{},{},{},{}]",
+                          v.v.slot,
+                          v.v.head.slot,
+                          v.v.target.slot,
+                          v.v.source.slot);
+  }
+};
+template <>
 struct fmt::formatter<lean::Debug<lean::Attestation>> {
   constexpr auto parse(const auto &ctx) {
     return ctx.end();
   }
   auto format(const lean::Debug<lean::Attestation> &v,
               fmt::format_context &ctx) const {
-    return fmt::format_to(ctx.out(),
-                          "[{}-{}-{}-{}-{}]",
-                          v.v.validator_id,
-                          v.v.data.slot,
-                          v.v.data.head.slot,
-                          v.v.data.target.slot,
-                          v.v.data.source.slot);
+    return fmt::format_to(
+        ctx.out(), "[{},{}]", v.v.validator_id, lean::Debug{v.v.data});
+  }
+};
+
+template <>
+struct fmt::formatter<lean::Debug<lean::AggregatedAttestation>> {
+  constexpr auto parse(const auto &ctx) {
+    return ctx.end();
+  }
+  auto format(const lean::Debug<lean::AggregatedAttestation> &v,
+              fmt::format_context &ctx) const {
+    return fmt::format_to(
+        ctx.out(),
+        "[[{}],{}]",
+        fmt::join(lean::getAggregatedValidators(v.v.aggregation_bits), ","),
+        lean::Debug{v.v.data});
   }
 };
 
@@ -48,9 +73,10 @@ struct fmt::formatter<lean::Debug<lean::BlockWithAttestation>> {
         v.v.block.hash(),
         v.v.block.parent_root,
         fmt::join(v.v.block.body.attestations.data()
-                      | std::views::transform([](const lean::Attestation &v) {
-                          return lean::Debug{v};
-                        }),
+                      | std::views::transform(
+                          [](const lean::AggregatedAttestation &v) {
+                            return lean::Debug{v};
+                          }),
                   "-"),
         lean::Debug{v.v.proposer_attestation});
   }
