7e369e0427210d1df5fe67746e5aef2e19375d1a
turuslan <turuslan.devbox@gmail.com>
2026-01-22 19:47:51 +0500

    update crates
    Signed-off-by: turuslan <turuslan.devbox@gmail.com>

--- vcpkg-overlay/qdrvm-crates/portfile.cmake
+++ vcpkg-overlay/qdrvm-crates/portfile.cmake
@@ -2,8 +2,8 @@ vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
 vcpkg_from_github(
   OUT_SOURCE_PATH SOURCE_PATH
   REPO qdrvm/qdrvm-crates
-  REF refs/tags/v1.0.7
-  SHA512 fe26cfb76ef48bdb8a71637afa2b6e176423b20f157abe226a3bd368eabcfda37ad054de7efb1fea93d8bf3d74b90db7fd295dbe0224a47f6384c43dc6dd33f3
+  REF refs/tags/v1.0.8
+  SHA512 b6422376099c788221d6b177b82304e769a26c291e9366dd16b9d7a928dd9573465eebc2060c8f997475dcd0d415a6c51872fdf3ad12fe4d1cad5f826e620886
 )
 vcpkg_cmake_configure(SOURCE_PATH "${SOURCE_PATH}" OPTIONS
   "-DQDRVM_BIND_CRATES=c_hash_sig"
