ccd0f646777a9a151586f9ad2c85606b71fd35af
Dmitriy Khaustov aka xDimon <khaustov.dm@gmail.com>
2026-02-03 17:18:09 +0800

    update leanp2p
    Signed-off-by: Dmitriy Khaustov aka xDimon <khaustov.dm@gmail.com>

--- vcpkg-overlay/leanp2p/portfile.cmake
+++ vcpkg-overlay/leanp2p/portfile.cmake
@@ -2,8 +2,8 @@ vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
 vcpkg_from_github(
   OUT_SOURCE_PATH SOURCE_PATH
   REPO qdrvm/leanp2p
-  REF 1a46fef5262952ef5e0f29451a6050f684c3cdae
-  SHA512 add73ce9b0428c911c9ec7134cd20885335d2611ea90e86e91eacb653671739f1c263a16a69225ad565ec6541e437bdd0cf1754b9b27e48a9edcd73f42e08dad
+  REF refs/tags/v0.0.7
+  SHA512 ab84dbcaf6babbe189bdb9cff7fa7763a3a07e6d47ba79e3f7a12c18583d322ce8028553903b3b791b127f67667846a27c884165b8dfc9cfbfa2e0c933cb6f54
 )
 vcpkg_cmake_configure(SOURCE_PATH "${SOURCE_PATH}")
 vcpkg_cmake_install()
