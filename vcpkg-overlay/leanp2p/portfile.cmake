6249deb7b5a08cceb14c1cfc19d81c297744c952
Dmitriy Khaustov aka xDimon <khaustov.dm@gmail.com>
2026-02-06 15:14:26 +0800

    vcpkg
    Signed-off-by: Dmitriy Khaustov aka xDimon <khaustov.dm@gmail.com>

--- vcpkg-overlay/leanp2p/portfile.cmake
+++ vcpkg-overlay/leanp2p/portfile.cmake
@@ -2,8 +2,8 @@ vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
 vcpkg_from_github(
   OUT_SOURCE_PATH SOURCE_PATH
   REPO qdrvm/leanp2p
-  REF 7ab988c0328478483204cf9140a4d894b38fe892 #refs/tags/v0.0.7
-  SHA512 1eb5f266e495ba3188ea7fa346e5562ee9fec8b2b7f4a75a38b37e64890494e7f95b04d0a15e4662607267ed0815a0710ddcfe1f72d08af6d26deba3e702530d
+  REF refs/tags/v0.0.8
+  SHA512 541ee0dcaa26a464e74db3a86dec930e2d25d04f329bd7bebf1788f29a455ba0f1642076b3c5e4bc83a80442039e67c6add8270c88f69a1748b2737adcff66a6
 )
 vcpkg_cmake_configure(SOURCE_PATH "${SOURCE_PATH}")
 vcpkg_cmake_install()
