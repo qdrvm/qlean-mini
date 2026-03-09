b2eed899d825bb6eb99eead5a96eb71564702d0d
turuslan <turuslan.devbox@gmail.com>
2026-03-09 16:10:39 +0500

    backward stack
    Signed-off-by: turuslan <turuslan.devbox@gmail.com>

--- src/executable/lean_node.cpp
+++ src/executable/lean_node.cpp
@@ -4,6 +4,7 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
+#include <backward.hpp>
 #include <chrono>
 #include <iostream>
 #include <memory>
@@ -105,6 +106,8 @@ namespace {
 int main(int argc, const char **argv, const char **env) {
   setlinebuf(stdout);
   setlinebuf(stderr);
+
+  backward::SignalHandling print_stack_on_signal;
 
   soralog::util::setThreadName("lean-node");
 
