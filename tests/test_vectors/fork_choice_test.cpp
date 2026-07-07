fd8be96972c103854de5263a35525ea5798eea4c
turuslan <turuslan.devbox@gmail.com>
2026-07-07 10:37:14 +0500

    test_aggregated_attestation_at_disparity_boundary_allowed
    Signed-off-by: turuslan <turuslan.devbox@gmail.com>

--- tests/test_vectors/fork_choice_test.cpp
+++ tests/test_vectors/fork_choice_test.cpp
@@ -39,7 +39,6 @@ TEST_P(ForkChoiceTest, ForkChoice) {
       "tests/consensus/lstar/fc/test_finalization_mid_processing.py::test_finalization_advances_mid_attestation_processing[fork_Lstar][fork_Lstar-fork_choice_test]",
 
       // TODO: HIVE
-      "tests/consensus/lstar/fc/test_gossip_aggregated_attestation_validation.py::test_aggregated_attestation_at_disparity_boundary_allowed[fork_Lstar][fork_Lstar-fork_choice_test]",
       "tests/consensus/lstar/fc/test_gossip_attestation_validation.py::test_attestation_just_beyond_disparity_boundary_rejected[fork_Lstar][fork_Lstar-fork_choice_test]",
       "tests/consensus/lstar/fc/test_gossip_attestation_validation.py::test_attestation_one_full_slot_in_future_rejected[fork_Lstar][fork_Lstar-fork_choice_test]",
       "tests/consensus/lstar/fc/test_tick_system.py::test_tick_interval_0_skips_acceptance_when_not_proposer[fork_Lstar][fork_Lstar-fork_choice_test]",
